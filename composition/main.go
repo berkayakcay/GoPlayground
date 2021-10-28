564babb1ca2b8ef3190609928ed4286e31b35ea3
berkayakcay <berkayakcay@outlook.com>
2021-10-28 15:24:21 +0300

    composition

---
+++ composition/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Vehicle struct {
+	NumberOfWheels     int
+	NumberOfPassengers int
+}
+
+type Car struct {
+	Make       string
+	Model      string
+	Year       int
+	IsElectric bool
+	IsHybrid   bool
+	Vehicle    Vehicle // Composition
+}
+
+func (v Vehicle) showDetails() {
+	fmt.Println("Number of passengers:", v.NumberOfPassengers)
+	fmt.Println("Number of wheels:", v.NumberOfWheels)
+}
+
+func (c Car) show() {
+	fmt.Println("Make:", c.Make)
+	fmt.Println("Model:", c.Model)
+	fmt.Println("Year:", c.Year)
+	fmt.Println("IsElectric:", c.IsElectric)
+	fmt.Println("IsHybrid:", c.IsHybrid)
+	c.Vehicle.showDetails()
+}
+
+func main() {
+
+	suv := Vehicle{
+		NumberOfWheels:     4,
+		NumberOfPassengers: 6,
+	}
+
+	honda := Car{
+		Make:       "Honda",
+		Model:      "Civic",
+		Year:       2021,
+		IsElectric: false,
+		IsHybrid:   false,
+		Vehicle:    suv,
+	}
+
+	honda.show()
+}
