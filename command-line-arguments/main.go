6b4434fe7882a5e46fdf1e99554f292bc0f43020
berkayakcay <berkayakcay@outlook.com>
2021-11-03 19:20:15 +0300

    command line arguments

---
+++ command-line-arguments/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("%#v\n", os.Args)
+
+	fmt.Println("Path:", os.Args[0])
+
+	fmt.Println("Number of items inside os.Args:", len(os.Args))
+}
